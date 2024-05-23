69f5129428e3963ba04b4eb9e3652127e65d73e8
Subir Mansukhani <subir.m@gmail.com>
2024-05-22 18:26:03 -0700

    add main

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ from graph_operations.graph_utils import save_nodes_to_csv, visualize_graph, get
     check_goals_and_run_pagerank, display_pagerank_scores
 from llm_chain.node_name_extraction import extract_goal_tech
 
-file_path = '/mnt/code/data/aiidb_full.json'
+file_path = 'data/aiidb_full.json'
 incident_data = load_json_data(file_path)
 # incident_data = sample_incidents(incident_data, n=30)
 G = build_graph(incident_data)
@@ -27,3 +27,7 @@ def failure_list(description: str, top_percentile: int = 85):
                 {"status": "success", "data": get_top_percentile_scores(result, percentile=top_percentile)})
     else:
         return json.dumps({"status": "info", "message": "No goals or technologies extracted."})
+
+
+if __name__ == "__main__":
+    print(failure_list("We have developed a chatbot that uses a transformer model for question answering."))
